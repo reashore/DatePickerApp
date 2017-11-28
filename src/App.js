e048fed32eb4944eb7fe511b2cc35ee38092403c
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-28 14:40:41 -0800

    Working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
+
+// https://react.rocks/example/react-datepicker
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment()
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Date Picker App</h1>
+
+        <DatePicker
+          dateFormat="YYYY/MM/DD"
+          selected={this.state.startDate}
+          onChange={this.onChange} />
       </div>
     );
   }
